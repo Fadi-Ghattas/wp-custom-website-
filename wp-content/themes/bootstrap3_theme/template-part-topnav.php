9cdf35882197e9bac3ebb36ad6cb55cde923161a
Mohammed Ousso <oussou@cloudappers.com>
2017-05-01 14:47:34 +0300

    add menu layout

--- wp-content/themes/bootstrap3_theme/template-part-topnav.php
+++ wp-content/themes/bootstrap3_theme/template-part-topnav.php
@@ -5,6 +5,12 @@
         <nav class="navbar navbar-inverse" role="navigation">
             <div class="container">
                 <div class="navbar-header">
+                    <a class="logo-mob hidden-lg hidden-md hidden-sm" href="<?php echo home_url(); ?>">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/img/blue-CA-icon.svg'  ?>" alt="CloudAppers" />
+                    </a>
+                    <a class="logo-desk" href="<?php echo home_url(); ?>">
+                        <img src="<?php echo get_stylesheet_directory_uri() . '/img/CA-full-logo@3x.png'  ?>" alt="CloudAppers" />
+                    </a>
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-1-collapse">
                         <span class="sr-only"><?php _e('Toggle navigation','devdmbootstrap3'); ?></span>
                         <span class="icon-bar"></span>
