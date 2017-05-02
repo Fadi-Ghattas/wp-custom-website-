7b9a81db642967bf89a272cda9378e84047ae3f8
Mohammed Ousso <oussou@cloudappers.com>
2017-05-02 18:14:04 +0300

    add what-we-do section

--- wp-content/themes/cloudappers_theme/index.php
+++ wp-content/themes/cloudappers_theme/index.php
@@ -137,6 +137,46 @@
     <div class="view-all"><a class="" href="">VIEW ALL CLIENTS</a></div>
 </section>
 
+<section class="what-we-do">
+    <div class="container">
+        <div class="row">
+            <div class="app col-lg-4">
+                <a href="#">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/service-nativeapp-icon@3x.svg' ?>"/>
+                    <h1>Native App Design & Development</h1>
+                    <p>Are you ready to become the next App Store hit with thousands of downloads per day? We build
+                        killer
+                        iOS and Android apps.
+                    </p>
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/more-grey-icon@3x.svg' ?>"/>
+                </a>
+            </div>
+            <div class="design col-lg-4">
+                <a href="#">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/service-ux-ui-icon@3x.svg' ?>"/>
+                    <h1>UX and UI Design</h1>
+                    <p>If you're looking to build a product that is available to users accross all platforms and
+                        devices, a
+                        web app is the answer.
+                    </p>
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/more-grey-icon@3x.svg' ?> "/>
+                </a>
+            </div>
+            <div class="consultancy col-lg-4">
+                <a href="#">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/service-marketing-icon@3x.svg' ?>"/>
+                    <h1>Digital Consultancy</h1>
+                    <p>We help companies close the gap between what consumers demand and expect digitally from
+                        businesses
+                        and what they actually experience.
+                    </p>
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/more-grey-icon@3x.svg' ?> "/>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php get_footer(); ?>
 
