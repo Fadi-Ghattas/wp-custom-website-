ceb55f2cb0fbed736736733aa3fe01bc6556868b
Mohammed Ousso <oussou@cloudappers.com>
2017-05-03 23:20:12 +0300

    update css Ver

--- wp-content/themes/cloudappers_theme/php/queue.php
+++ wp-content/themes/cloudappers_theme/php/queue.php
@@ -18,7 +18,7 @@ function project_scripts()
 //	wp_enqueue_style('sweetalert2', get_template_directory_uri() . '/css/sweetalert2/sweetalert2.min.css', [], '3.4');
 	wp_enqueue_style('slick-css', get_template_directory_uri() . '/css/slick-slider/slick.css', [], '3.4');
 	wp_enqueue_style('slick-theme', get_template_directory_uri() . '/css/slick-slider/slick-theme.css', [], '3.4');
-	wp_enqueue_style('layout', get_template_directory_uri() . '/css/custom/default-style.css', [], '1.0');
+	wp_enqueue_style('layout', get_template_directory_uri() . '/css/custom/default-style.css', [], '1.1');
 
 	//JQUERY
 	wp_enqueue_script('jquery-min', get_template_directory_uri() . '/js/jquery/jquery-2.2.2.min.js', [], '2.2.2', FALSE);
