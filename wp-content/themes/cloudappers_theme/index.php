56f2cd082379013f990cc2274198b4fe1c6b8934
Mohammed Ousso <oussou@cloudappers.com>
2017-05-15 11:30:35 +0300

    menu > fix loader layout

--- wp-content/themes/cloudappers_theme/index.php
+++ wp-content/themes/cloudappers_theme/index.php
@@ -231,7 +231,7 @@ get_template_part('template-part', 'topnav');
                 foreach ($team as $teamMember) {
                     $hover = ['gradient-purple', 'gradient-pink', 'gradient-green'];
                     ?>
-                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+                    <div class="responsive-loader col-lg-3 col-md-4 col-sm-6 col-xs-6">
                         <section class="<?php echo $hover[$hoverCount]; ?>">
                             <figure>
                             <!--<div class="lazy-loader-effect"></div>-->
