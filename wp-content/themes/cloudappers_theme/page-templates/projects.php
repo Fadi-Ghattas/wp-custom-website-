1a687daf7a3ba47bb172f58f31031637ccecf458
Fadi <fadi@cloudappers.com>
2017-05-16 10:48:16 +0300

    update show you page filters and css

--- wp-content/themes/cloudappers_theme/page-templates/projects.php
+++ wp-content/themes/cloudappers_theme/page-templates/projects.php
@@ -41,7 +41,7 @@ get_template_part('template-part', 'topnav');
 				<div class="filters-back">
 					<div class="filters-container">
 						<ul class="filters">
-							<li><a href="#">
+							<li><a href="#" class="active">
 									<img class="icon" src="<?php echo esc_url(get_stylesheet_directory_uri(). '/img/all-icon.png'); ?>">
 									<img class="icon-hover" src="<?php echo esc_url(get_stylesheet_directory_uri(). '/img/all-icon.hover.png');?>">
 									ALL
@@ -92,6 +92,21 @@ get_template_part('template-part', 'topnav');
 		</div>
 	</div>
 </section>
+
+	<section class="prefooter lazy-background" data-bg="<?php echo esc_url(get_stylesheet_directory_uri() . '/img/prefooter.png'); ?>">
+		<div class="container">
+			<div class="row">
+				<div class="img-prefooter col-lg-5  col-md-12">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri().'/img/infographics-for-banner@3x.png' ?>">
+				</div>
+				<div class="col-lg-7  col-md-12">
+					<h1><?php echo $setting['settings_pre_footer_title']; ?></h1>
+					<p><?php echo $setting['settings_pre_footer_subtitle']; ?></p>
+					<a href="" class="c-btn">TELL US ABOUT YOUR PROJECT</a>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php
 get_footer();
