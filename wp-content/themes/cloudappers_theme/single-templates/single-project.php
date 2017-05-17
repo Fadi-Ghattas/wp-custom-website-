36903142e52a9fef244d96ec08b0726c1ab831a2
Fadi <fadi@cloudappers.com>
2017-05-17 14:01:01 +0300

    update single project page

--- wp-content/themes/cloudappers_theme/single-templates/single-project.php
+++ wp-content/themes/cloudappers_theme/single-templates/single-project.php
@@ -5,3 +5,201 @@
  * Date: 05/11/17
  * Time: 1:15 PM
  */
+
+$setting = get_page_by_path('cloudappers-setting', OBJECT, 'page');
+$setting = acf_get_group_fields($setting->ID);
+
+$fields = [
+	'id',
+	'post_title',
+	[
+		'acf' => 1,
+		'name' => 'project_name_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_logo',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_header_title',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_header_title_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_header_background_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_background_header_image',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_short_description',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_short_description_color',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_info_blocks',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_slider',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_slider_section_background_color',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_one_image',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_one_image_background_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_one_description',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_one_description_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_one_description_background_color',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_two_title',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_two_title_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_two_background_image',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_two_description',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_show_case_two_description_color',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_client_say_blocks',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_client_says_section_background_type',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_client_says_section_background_gradient',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_client_says_section_background_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_client_says_section_background_image',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_url_title',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_url_title_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_url',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_url_name',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_url_name_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_url_section_background_color',
+	],
+
+	[
+		'acf' => 1,
+		'name' => 'project_next_project_link_text_color',
+	],
+	[
+		'acf' => 1,
+		'name' => 'project_page_separators_lines_color',
+	],
+
+];
+
+$project = ProjectModel::findOne((new ProjectModel())->pod_name, get_the_ID(), $fields);
+
+
+get_header();
+get_template_part('template-part', 'topnav');
+?>
+	<section class="project-header">
+		<div class="project-header-container">
+			<img class="header-image" src="<?php echo esc_url($project['project_background_header_image']['url']); ?>">
+			<div class="center header-text">
+				<p class="title" style="color: <?php echo $project['project_header_title_color']; ?>"><?php echo $project['project_header_title']; ?></p>
+				<p class="name" style="color: <?php echo $project['project_name_color']; ?>"><?php echo $project['post_title']; ?></p>
+				<div class="short-description" style="color: <?php echo $project['project_short_description_color']; ?>">
+					<?php echo $project['project_short_description']; ?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+<?php if (!empty($project['project_info_blocks'])) { ?>
+	<section class="project-info">
+		<div class="container">
+			<div class="row">
+				<?php foreach ($project['project_info_blocks'] as $info_block) { ?>
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+						<?php if($info_block['project_block_type'] == 1) { ?>
+							<p class="title" style="color: <?php echo $info_block['project_block_title_color']; ?>"><?php echo $info_block['project_block_title']; ?><p>
+							<div class="description" style="<?php echo $info_block['project_block_description_color'] ?>">
+								<?php echo $info_block['project_block_description'] ?>
+							</div>
+						<?php } else if ($info_block['project_block_type'] == 3) { ?>
+								<img src="<?php echo $project['project_logo']['url']; ?>" alt="<?php echo $project['post_title']; ?>">
+						<?php } else if ($info_block['project_block_type'] == 2) { ?>
+							<img src="<?php echo $info_block['project_block_image']['url']; ?>" alt="<?php echo $project['post_title']; ?>">
+						<?php } ?>
+					</div>
+				<?php } ?>
+			</div>
+		</div>
+	</section>
+<?php } ?>
+
+<?php
+get_footer();
