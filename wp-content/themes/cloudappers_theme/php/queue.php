1d7cacb77c62225a9f576b8d562f3d2371cdc395
Mohammed Ousso <oussou@cloudappers.com>
2017-06-07 13:45:50 +0300

    projects > fix layout

--- wp-content/themes/cloudappers_theme/php/queue.php
+++ wp-content/themes/cloudappers_theme/php/queue.php
@@ -49,7 +49,7 @@ function project_scripts()
 	}
 
 	//wp_enqueue_script('helpers_js', get_template_directory_uri() . '/js/custom/helpers.js', ['jquery-min'], '1.0', FALSE);
-	wp_enqueue_script('css_js', get_template_directory_uri() . '/js/custom/css.js', ['jquery-min'], '1.75', FALSE);
+	wp_enqueue_script('css_js', get_template_directory_uri() . '/js/custom/css.js', ['jquery-min'], '1.76', FALSE);
 	wp_enqueue_script('ajax_js', get_template_directory_uri() . '/js/custom/ajax.js', ['jquery-min'], '1.46', TRUE);
 	wp_enqueue_script('theme_js', get_template_directory_uri() . '/js/custom/theme.js', ['jquery-min'], '1.14', TRUE);
 
