60f92a5534aa98232f35c577af07e5512cf0f299
Fadi <fadi@cloudappers.com>
2017-06-07 12:12:48 +0300

    update services page

--- wp-content/themes/cloudappers_theme/js/custom/css.js
+++ wp-content/themes/cloudappers_theme/js/custom/css.js
@@ -211,6 +211,15 @@ jQuery(function ($) {
 
         });
 
+        $('ul.filters li a').on('click', function (e) {
+
+            $('ul.filters li a').parent().removeClass('active');
+            $(this).parent().addClass('active');
+            $isotope.isotope({filter: $(this).attr('data-type')});
+            return false;
+
+        });
+
         function ReLayout() {
             var col = 0;
             var defaultCol = 'col-xs-12 col-sm-6 col-md-4 col-lg-4 show-case-item zoom-effect ';
