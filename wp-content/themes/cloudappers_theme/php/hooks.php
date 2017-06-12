4d9dbd56617c02f10ea13b1adb538e80f72a34bd
Fadi <fadi@cloudappers.com>
2017-06-12 12:20:37 +0300

    update google maps

--- wp-content/themes/cloudappers_theme/php/hooks.php
+++ wp-content/themes/cloudappers_theme/php/hooks.php
@@ -8,19 +8,19 @@
 
 function project_custom_setup()
 {
-    //adding images sizes
-    add_theme_support('post-thumbnails');
+	//adding images sizes
+	add_theme_support('post-thumbnails');
 //    add_image_size('project-slide', 1170, 992, true);
 
-    //remove admin bar
-    //add_filter('show_admin_bar', '__return_false');
+	//remove admin bar
+	//add_filter('show_admin_bar', '__return_false');
 }
 
 //add_action( 'after_setup_theme', 'project_custom_setup' );
 
 function project_custom_login_logo()
 {
-    echo '<style type="text/css">
+	echo '<style type="text/css">
             h1 a { background-image: url(' . get_bloginfo('template_directory') . '/imgs/logo.png) !important; }
          </style>';
 }
@@ -28,38 +28,71 @@ function project_custom_login_logo()
 //add_action('login_head', 'project_custom_login_logo');
 
 
-add_action( 'init', 'remove_editor_init' );
+add_action('init', 'remove_editor_init');
 
-function remove_editor_init() {
+function remove_editor_init()
+{
 	// If not in the admin, return.
-	if ( ! is_admin() ) {
+	if (!is_admin()) {
 		return;
 	}
 
 	// Get the post ID on edit post with filter_input super global inspection.
-	$current_post_id = filter_input( INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT );
+	$current_post_id = filter_input(INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT);
 	// Get the post ID on update post with filter_input super global inspection.
-	$update_post_id = filter_input( INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT );
+	$update_post_id = filter_input(INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT);
 
 	// Check to see if the post ID is set, else return.
-	if ( isset( $current_post_id ) ) {
-		$post_id = absint( $current_post_id );
-	} else if ( isset( $update_post_id ) ) {
-		$post_id = absint( $update_post_id );
+	if (isset($current_post_id)) {
+		$post_id = absint($current_post_id);
+	} else if (isset($update_post_id)) {
+		$post_id = absint($update_post_id);
 	} else {
 		return;
 	}
 
 	// Don't do anything unless there is a post_id.
-	if ( isset( $post_id ) ) {
+	if (isset($post_id)) {
 		// Get the template of the current post.
-		$template_file = get_post_meta( $post_id, '_wp_page_template', true );
+		$template_file = get_post_meta($post_id, '_wp_page_template', TRUE);
 		$template_file = explode('/', $template_file);
 		// Example of removing page editor for page-your-template.php template.
-		$pages = ['services.php', 'home.php', 'clients.php', 'settings.php', 'contact-us.php', 'projects.php', 'about.php','team.php'];
-		if (  in_array($template_file[sizeof($template_file)-1] , $pages)) {
-			remove_post_type_support( 'page', 'editor' );
+		$pages = ['services.php', 'home.php', 'clients.php', 'settings.php', 'contact-us.php', 'projects.php', 'about.php', 'team.php'];
+		if (in_array($template_file[sizeof($template_file) - 1], $pages)) {
+			remove_post_type_support('page', 'editor');
 			// Other features can also be removed in addition to the editor. See: https://codex.wordpress.org/Function_Reference/remove_post_type_support.
 		}
 	}
 }
+
+
+function add_defer_attribute($tag, $handle)
+{
+	// add script handles to the array below
+	$scripts_to_defer = ['google_map', 'google_map_js'];
+
+	foreach ($scripts_to_defer as $defer_script) {
+		if ($defer_script === $handle) {
+			return str_replace(' src', ' defer="defer" src', $tag);
+		}
+	}
+	return $tag;
+}
+
+add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
+
+
+function add_async_attribute($tag, $handle)
+{
+	// add script handles to the array below
+	$scripts_to_async = ['google_map', 'google_map_js'];
+
+	foreach ($scripts_to_async as $async_script) {
+		if ($async_script === $handle) {
+			return str_replace(' src', ' async="async" src', $tag);
+		}
+	}
+	return $tag;
+}
+
+add_filter('script_loader_tag', 'add_async_attribute', 10, 2);
--- wp-content/themes/cloudappers_theme/php/queue.php
+++ wp-content/themes/cloudappers_theme/php/queue.php
@@ -20,15 +20,29 @@ function project_scripts()
 	wp_enqueue_style('formValidationcss', get_template_directory_uri() . '/css/bootstrap/bootstrap/formValidation.min.css', [], '1.0');
 	wp_enqueue_style('fontawesomecss', get_template_directory_uri() . '/css/font-awesome/font-awesome.min.css', [], '1.0');
 	wp_enqueue_style('slick-css', get_template_directory_uri() . '/css/slick-slider/slick.css', [], '3.4');
-	wp_enqueue_style('slick-theme', get_template_directory_uri() . '/css/slick-slider/slick-theme.css', [],	 '3.6');
-
-	if(is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
-		wp_enqueue_style('bootstrap-fileinput-css', get_template_directory_uri() . '/css/bootstrap/bootstrap-fileinput/css/fileinput.min.css', [], '1');
-	}
-
+	wp_enqueue_style('slick-theme', get_template_directory_uri() . '/css/slick-slider/slick-theme.css', [], '3.6');
+//	if(is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
+	wp_enqueue_style('bootstrap-fileinput-css', get_template_directory_uri() . '/css/bootstrap/bootstrap-fileinput/css/fileinput.min.css', [], '1');
+//	}
 	wp_enqueue_style('layout', get_template_directory_uri() . '/css/custom/default-style.css', [], '2.36');
 
 	//JQUERY
+	$script_const = [
+		'ajaxurl' => admin_url('admin-ajax.php'),
+		'ReCaptcha_SiteKey' => '6LdqzCAUAAAAADJJyW_TCrlAdCSGlxqWkZBoU4P9',
+		'error_message' => 'Something went wrong please try again later.',
+		'error_message_color' => '#a94442',
+		'page_template' => basename(get_page_template()),
+	];
+
+	if (is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
+		$homePagePost = get_page_by_path('home', OBJECT, 'page');
+		$pageOptions = acf_get_group_fields($homePagePost->ID);
+		$script_const['map_pins_url'] = esc_url($pageOptions['home_page_map_pins'][0]['home_page_map_pin_image']['url']);
+		$script_const['pin_latitude'] = $pageOptions['home_page_map_pins'][0]['home_page_map_pin_latitude'];
+		$script_const['pin_altitude'] = $pageOptions['home_page_map_pins'][0]['home_page_map_pin_altitude'];
+	}
+
 	wp_enqueue_script('jquery-min', get_template_directory_uri() . '/js/jquery/jquery-2.2.2.min.js', [], '2.2.2', FALSE);
 	wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap/bootstrap.min.js', ['jquery-min'], '2', FALSE);
 	wp_enqueue_script('lazyloadxt', get_template_directory_uri() . '/js/lazy-load-xt/jquery.lazyloadxt.min.js', ['jquery-min'], '1', FALSE);
@@ -40,26 +54,24 @@ function project_scripts()
 	wp_enqueue_script('slick', get_template_directory_uri() . '/js/slick-slider/slick.min.js', ['jquery-min'], '1', FALSE);
 	wp_enqueue_script('parallax', get_template_directory_uri() . '/js/parallax/parallax.min.js', ['jquery-min'], '1', FALSE);
 
-	if(is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
-		wp_enqueue_script('bootstrap-fileinput-js', get_template_directory_uri() . '/js/bootstrap/bootstrap-fileinput/js/fileinput.min.js', ['jquery-min', 'bootstrap'], '1', FALSE);
+	if (is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
+		wp_enqueue_script('google_map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDWs0rsi44WbJwTxkHdutuiLXXyQZ8pd68&callback', ['jquery-min'], '1', TRUE);
+		wp_enqueue_script('google_map_js', get_template_directory_uri() . '/js/custom/google_map.js', ['jquery-min', 'google_map'], '1.0', TRUE);
 	}
 
-	if (in_array(basename(get_page_template()), ['projects.php','team.php'])) {
-		wp_enqueue_script('bootstrap-isotope-js', get_template_directory_uri() . '/js/isotope/isotope.pkgd.min.js', ['jquery-min'], '1', FALSE);
-	}
+//	if(is_home() || in_array(basename(get_page_template()), ['contact-us.php'])) {
+	wp_enqueue_script('bootstrap-fileinput-js', get_template_directory_uri() . '/js/bootstrap/bootstrap-fileinput/js/fileinput.min.js', ['jquery-min', 'bootstrap'], '1', FALSE);
+//	}
+//	if (in_array(basename(get_page_template()), ['projects.php','team.php'])) {
+	wp_enqueue_script('bootstrap-isotope-js', get_template_directory_uri() . '/js/isotope/isotope.pkgd.min.js', ['jquery-min'], '1', FALSE);
+//	}
 
 	//wp_enqueue_script('helpers_js', get_template_directory_uri() . '/js/custom/helpers.js', ['jquery-min'], '1.0', FALSE);
 	wp_enqueue_script('css_js', get_template_directory_uri() . '/js/custom/css.js', ['jquery-min'], '1.92', FALSE);
 	wp_enqueue_script('ajax_js', get_template_directory_uri() . '/js/custom/ajax.js', ['jquery-min'], '1.48', TRUE);
 	wp_enqueue_script('theme_js', get_template_directory_uri() . '/js/custom/theme.js', ['jquery-min'], '1.16', TRUE);
 
-	wp_localize_script('theme_js', 'script_const', [
-		'ajaxurl' => admin_url('admin-ajax.php'),
-		'ReCaptcha_SiteKey' => '6LdqzCAUAAAAADJJyW_TCrlAdCSGlxqWkZBoU4P9',
-		'error_message' => 'Something went wrong please try again later.',
-		'error_message_color' => '#a94442',
-		'page_template' => basename(get_page_template())
-	]);
+	wp_localize_script('theme_js', 'script_const', $script_const);
 }
 
 add_action('wp_enqueue_scripts', 'project_scripts');
