3a1ff16a5254dbfc9f7093c7d7ee95ee3bc406a7
Rahaf <rahaf@cloudappers.com>
2017-06-14 13:46:46 +0300

    Merge branch 'master' of git.cloudappers.com:cloudappers/cloudappers-2017
    # Conflicts:
    #	wp-content/themes/cloudappers_theme/php/queue.php

--- wp-content/themes/cloudappers_theme/js/custom/theme.js
+++ wp-content/themes/cloudappers_theme/js/custom/theme.js
@@ -27,5 +27,42 @@ jQuery(function ($) {
 	//	$('body').unbind('touchmove');
 	//	console.log('unbind');
 	//})
+	document.ontouchmove = function ( event ) {
+
+		var isTouchMoveAllowed = true, target = event.target;
+
+		while ( target !== null ) {
+			if ( target.classList && target.classList.contains( 'modal-open' ) ) {
+				isTouchMoveAllowed = false;
+				break;
+			}
+			target = target.parentNode;
+		}
+
+		if ( !isTouchMoveAllowed ) {
+			event.preventDefault();
+		}
+
+	};
+
+
+
+	function removeIOSRubberEffect( element ) {
+
+		element.addEventListener( "touchstart", function () {
+
+			var top = element.scrollTop, totalScroll = element.scrollHeight, currentScroll = top + element.offsetHeight;
+
+			if ( top === 0 ) {
+				element.scrollTop = 1;
+			} else if ( currentScroll === totalScroll ) {
+				element.scrollTop = top - 1;
+			}
+
+		} );
+
+	}
+
+	removeIOSRubberEffect( document.querySelector( ".modal" ) );
 
 });
