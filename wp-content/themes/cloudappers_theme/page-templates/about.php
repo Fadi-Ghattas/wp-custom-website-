29fa350c8fecba6a3d632c8e18fbfa10f010203f
Mohammed Ousso <oussou@cloudappers.com>
2017-06-14 12:25:11 +0300

    fix links

--- wp-content/themes/cloudappers_theme/page-templates/about.php
+++ wp-content/themes/cloudappers_theme/page-templates/about.php
@@ -179,7 +179,7 @@ get_template_part('template-part', 'topnav');
                 <div class="col-lg-7  col-md-12">
                     <h1><?php echo $setting['settings_pre_footer_title']; ?></h1>
                     <p><?php echo $setting['settings_pre_footer_subtitle']; ?></p>
-                    <a href="https://cloudappers.typeform.com/to/dUDCpe" class="c-btn">TELL US ABOUT YOUR PROJECT</a>
+                    <a target="_blank" href="https://cloudappers.typeform.com/to/dUDCpe" class="c-btn">TELL US ABOUT YOUR PROJECT</a>
                 </div>
             </div>
         </div>
