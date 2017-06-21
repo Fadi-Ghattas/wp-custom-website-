baab9b1b9f7d258c9337ce88c30426e661e0491b
Mohammed Ousso <oussou@cloudappers.com>
2017-06-21 18:31:53 +0300

    fix

--- wp-content/themes/cloudappers_theme/page-templates/team.php
+++ wp-content/themes/cloudappers_theme/page-templates/team.php
@@ -94,7 +94,7 @@ get_template_part('template-part', 'topnav');
                                     </figure>
                                     <div class="overlay member-info">
                                         <h5><?php echo $teamMember['post_title']; ?></h5>
-                                        <h6><?php echo $teamMember['team_member_position']; ?></h6>
+                                        <h6><?php echo $teamMember['team_member_title']; ?></h6>
                                     </div>
                                 </section>
                             </div>
