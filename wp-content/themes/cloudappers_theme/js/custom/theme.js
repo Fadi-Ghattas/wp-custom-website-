ab6a173745a72e2721481c51f9674dfdb6f0dbd7
Mohammed Ousso <oussou@cloudappers.com>
2017-06-22 13:12:03 +0300

    fix

--- wp-content/themes/cloudappers_theme/js/custom/theme.js
+++ wp-content/themes/cloudappers_theme/js/custom/theme.js
@@ -1,6 +1,8 @@
 jQuery(function ($) {
 
-
+    $('#take-me-in').on('click', function (e) {
+        $('#JobModal').modal('show');
+    });
 
     $('#take-me-there').on('click', function (e) {
         $('#map-popup').modal('show');
