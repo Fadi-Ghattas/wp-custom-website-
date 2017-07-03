02cabefcd47555b48e59d1d3dd8e3552543f036c
Fadi <fadi@cloudappers.com>
2017-07-03 15:33:05 +0300

    udpate

--- wp-content/themes/cloudappers_theme/js/custom/google_map.js
+++ wp-content/themes/cloudappers_theme/js/custom/google_map.js
@@ -144,11 +144,11 @@ jQuery(function ($)
             // map1.setCenter(marker1.getPosition());
         });
 
-		$('.map-mob-shadow').on('click touchstart', function() {
+		$('.map-mob-shadow').on('click touchstart', function(event) {
 			infowindow.close(map1, marker1);
 		});
 
-		google.maps.event.addListener(map1, "click touchstart", function(event) {
+		google.maps.event.addListener(map1,'click touchstart', function(event) {
 			infowindow.close(map1, marker1);
 		});
 
