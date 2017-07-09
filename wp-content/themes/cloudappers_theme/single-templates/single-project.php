c12ec3828dce38958b6e46dd45798da77d6bc55e
Fadi <fadi@cloudappers.com>
2017-07-09 11:52:14 +0300

    update singe project

--- wp-content/themes/cloudappers_theme/single-templates/single-project.php
+++ wp-content/themes/cloudappers_theme/single-templates/single-project.php
@@ -273,11 +273,13 @@ get_template_part('template-part', 'topnav');
 				</div>
 			</div>
 		</div>
+	<?php if(!empty($project['project_show_case_one_description'])) { ?>
         <a href="#" class="move-section mob" data-index="2">
             <div >
                 <div class="icono-arrow1-down"></div>
             </div>
         </a>
+	<?php } ?>
 	</section>
 <?php } ?>
 
