c98a05119ee50704d747ea3ab34f4d3cc9f02a16
Fadi <fadi@cloudappers.com>
2017-07-09 11:18:22 +0300

    update wordpress

--- wp-includes/theme-compat/header.php
+++ wp-includes/theme-compat/header.php
@@ -19,15 +19,12 @@ _deprecated_file(
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 <title><?php echo wp_get_document_title(); ?></title>
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 
 <?php if ( file_exists( get_stylesheet_directory() . '/images/kubrickbgwide.jpg' ) ) { ?>
 <style type="text/css" media="screen">
