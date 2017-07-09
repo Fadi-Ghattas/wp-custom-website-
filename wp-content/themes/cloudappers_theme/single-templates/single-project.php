1f112c4dcf41c192a3f5051504001bb3c8f662b5
Mohammed Ousso <oussou@cloudappers.com>
2017-07-09 13:41:49 +0300

    project > fix mobile layout

--- wp-content/themes/cloudappers_theme/single-templates/single-project.php
+++ wp-content/themes/cloudappers_theme/single-templates/single-project.php
@@ -334,7 +334,9 @@ get_template_part('template-part', 'topnav');
                 </div>
             </div>
         </div>
-        <img class="img-responsive" src="<?php echo esc_url($project['project_show_case_two_background_image']['url']); ?>">
+        <div class="duble-container">
+            <img class="img-responsive" src="<?php echo esc_url($project['project_show_case_two_background_image']['url']); ?>">
+        </div>
 	</section>
 <?php } ?>
 
