ecb9f2b3b0e9f0a2385904a5c92eacbdd8c075bb
Mohammed Ousso <oussou@cloudappers.com>
2017-07-11 17:47:18 +0300

    update

--- wp-content/themes/cloudappers_theme/template-part-topnav.php
+++ wp-content/themes/cloudappers_theme/template-part-topnav.php
@@ -2,9 +2,6 @@
 
     <section class="top-menu">
         <div class="top-header">
-            <a href="<?php echo esc_url(home_url('services')); ?>/" class="service-back btn-view-all-show-cases c-btn sold-btn">
-                BACK TO WHAT WE CAN DO
-            </a>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -60,6 +57,9 @@
                     </div>
                 </div>
             </div>
+            <a href="<?php echo esc_url(home_url('services')); ?>/" class="service-back btn-view-all-show-cases c-btn sold-btn">
+                BACK TO WHAT WE CAN DO
+            </a>
         </div>
 
     </section>
