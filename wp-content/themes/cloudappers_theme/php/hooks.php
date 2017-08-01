fe0e666f4e96f97fd02e5513ae1bde7025ecab48
Fadi <fadi@cloudappers.com>
2017-08-01 16:49:44 +0300

    add plugins

--- wp-content/themes/cloudappers_theme/php/hooks.php
+++ wp-content/themes/cloudappers_theme/php/hooks.php
@@ -89,41 +89,41 @@ function custom_loginlogo_url($url) {
 }
 add_filter( 'login_headerurl', 'custom_loginlogo_url' );
 
-function remove_editor_init()
-{
-	// If not in the admin, return.
-	if (!is_admin()) {
-		return;
-	}
-
-	// Get the post ID on edit post with filter_input super global inspection.
-	$current_post_id = filter_input(INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT);
-	// Get the post ID on update post with filter_input super global inspection.
-	$update_post_id = filter_input(INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT);
-
-	// Check to see if the post ID is set, else return.
-	if (isset($current_post_id)) {
-		$post_id = absint($current_post_id);
-	} else if (isset($update_post_id)) {
-		$post_id = absint($update_post_id);
-	} else {
-		return;
-	}
-
-	// Don't do anything unless there is a post_id.
-	if (isset($post_id)) {
-		// Get the template of the current post.
-		$template_file = get_post_meta($post_id, '_wp_page_template', TRUE);
-		$template_file = explode('/', $template_file);
-		// Example of removing page editor for page-your-template.php template.
-		$pages = ['clients.php', 'settings.php', 'contact-us.php', 'about.php', 'team.php'];
-		if (in_array($template_file[sizeof($template_file) - 1], $pages)) {
-			remove_post_type_support('page', 'editor');
-			// Other features can also be removed in addition to the editor. See: https://codex.wordpress.org/Function_Reference/remove_post_type_support.
-		}
-	}
-}
-add_action('init', 'remove_editor_init');
+//function remove_editor_init()
+//{
+//	// If not in the admin, return.
+//	if (!is_admin()) {
+//		return;
+//	}
+//
+//	// Get the post ID on edit post with filter_input super global inspection.
+//	$current_post_id = filter_input(INPUT_GET, 'post', FILTER_SANITIZE_NUMBER_INT);
+//	// Get the post ID on update post with filter_input super global inspection.
+//	$update_post_id = filter_input(INPUT_POST, 'post_ID', FILTER_SANITIZE_NUMBER_INT);
+//
+//	// Check to see if the post ID is set, else return.
+//	if (isset($current_post_id)) {
+//		$post_id = absint($current_post_id);
+//	} else if (isset($update_post_id)) {
+//		$post_id = absint($update_post_id);
+//	} else {
+//		return;
+//	}
+//
+//	// Don't do anything unless there is a post_id.
+//	if (isset($post_id)) {
+//		// Get the template of the current post.
+//		$template_file = get_post_meta($post_id, '_wp_page_template', TRUE);
+//		$template_file = explode('/', $template_file);
+//		// Example of removing page editor for page-your-template.php template.
+//		$pages = ['clients.php', 'settings.php', 'contact-us.php', 'about.php', 'team.php'];
+//		if (in_array($template_file[sizeof($template_file) - 1], $pages)) {
+//			remove_post_type_support('page', 'editor');
+//			// Other features can also be removed in addition to the editor. See: https://codex.wordpress.org/Function_Reference/remove_post_type_support.
+//		}
+//	}
+//}
+//add_action('init', 'remove_editor_init');
 
 function add_defer_attribute($tag, $handle)
 {
